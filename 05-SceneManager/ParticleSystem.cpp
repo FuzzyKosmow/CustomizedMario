f2968d5161c26025459478a6e28a1b3a31bec193
Shoru <bapcui596@gmail.com>
2023-06-30 05:59:09 +0700

    Static spawn of particle system

--- 05-SceneManager/ParticleSystem.cpp
+++ 05-SceneManager/ParticleSystem.cpp
@@ -1,5 +1,9 @@
 #include "ParticleSystem.h"
-
+#include "PlayScene.h"
+#include <cstdlib>
+#include <random>
+#include <algorithm>
+using namespace std;
 void Particle::Render()
 {
 	if (isSprite)
@@ -16,3 +20,123 @@ void Particle::Render()
 	RenderBoundingBox();
 }
 
+
+
+void ParticleSystem::Update (DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	for (int i = 0; i < particles.size(); i++)
+	{
+		if (particles[i]->IsDeleted())
+		{
+			particles.erase(particles.begin() + i);
+			//Safe guard to make sure no funny business happens
+			if (spawned >0)
+				spawned--;
+			continue;
+		}
+	}
+}
+//Count must be bigger than 4.
+// X,y is supposed where the system will spawn. It can then be modified the center point spawn of the particle using the spawn offset
+// Spawn a <count> number of particles in 4 main different direction ( Top left, top right, bottom left, bottom right). Each direction consist at least 1 particle, maximum of (count -3)
+// And will be randomized from 1 -> (count -3 - spawned) for each direction.
+void ParticleSystem::SpawnStaticParticle(float x, float y, int count, int spriteID)
+{
+	//Seed the rand
+	srand(time(NULL));
+	
+	if (count < 4)
+		return;
+
+	int maxParticles = count - 3;
+
+	int topLeftSpawnCount = 1;
+	int topRightSpawnCount = 1;
+	int remainingCount = 2;
+
+	if (count > 4) {
+		// Generate random values for each direction
+		topLeftSpawnCount = rand() % (maxParticles - 1) + 1;
+		topRightSpawnCount = rand() % (maxParticles - topLeftSpawnCount) + 1;
+		remainingCount = maxParticles - topLeftSpawnCount - topRightSpawnCount;
+
+		// Ensure the remaining count is within the valid range
+		remainingCount = min(remainingCount, maxParticles - 1);
+		remainingCount = max(remainingCount, 1);
+	}
+
+
+	int bottomLeftSpawnCount = rand () % remainingCount + 1;
+	int bottomRightSpawnCount = maxParticles - topLeftSpawnCount - topRightSpawnCount - bottomLeftSpawnCount;
+
+
+	//Top left 
+	D3DXVECTOR2 direction;
+	LPPLAYSCENE scene = (LPPLAYSCENE)CGame::GetInstance()->GetCurrentScene();
+	for (int i = 0;  i< topLeftSpawnCount; i++)
+	{
+		direction = D3DXVECTOR2(-1, -1);
+		
+
+		//Randomize the direction from 0.0 -> 2.0. Make it float
+		direction.x = ((rand() % 100 + 1) / 100.0f) * direction.x;
+		direction.y = ((rand() % 100 + 1) / 100.0f) * direction.y;
+		Particle* p = new Particle( x, y , direction * spawnPower , spriteID);
+		
+		scene ->AddObject(p);
+		particles.push_back(p);
+		spawned++;
+	}
+	//Top right
+	for (int i = 0; i < topRightSpawnCount; i++)
+	{
+		direction = D3DXVECTOR2(1, -1);
+		direction.x = ((rand() % 100 + 1) / 100.0f) * direction.x;
+		direction.y = ((rand() % 100 + 1) / 100.0f) * direction.y;
+		Particle* p = new Particle(x, y, direction * spawnPower, spriteID);
+
+		scene->AddObject(p);
+		particles.push_back(p);
+		spawned++;
+	}
+	//Bottom left
+	for (int i = 0; i < bottomLeftSpawnCount; i++)
+	{
+		direction = D3DXVECTOR2(-1, 1);
+		direction.x = ((rand() % 100 + 1) / 100.0f) * direction.x;
+		direction.y = ((rand() % 100 + 1) / 100.0f) * direction.y;
+		Particle* p = new Particle(x, y, direction * spawnPower, spriteID);
+
+		scene->AddObject(p);
+		particles.push_back(p);
+		spawned++;
+	}
+	//Bottom right
+	for (int i = 0; i < bottomRightSpawnCount; i++)
+	{
+		direction = D3DXVECTOR2(1, 1);
+		direction.x = ((rand() % 100 + 1) / 100.0f) * direction.x;
+		direction.y = ((rand() % 100 + 1) / 100.0f) * direction.y;
+		Particle* p = new Particle(x, y, direction * spawnPower, spriteID);
+
+		scene->AddObject(p);
+		particles.push_back(p);
+		spawned++;
+	}
+
+}
+
+void ParticleSystem::SpawnAnimatedParticle(float x, float y, int count, int aniID)
+{
+
+}
+
+LPPARTICLESYSTEM ParticleSystem::GetInstance ()
+{
+	
+if (__instance == NULL)
+		__instance = new ParticleSystem(1,1);
+	return __instance;
+}
+
+LPPARTICLESYSTEM ParticleSystem::__instance = NULL;
--- 05-SceneManager/ParticleSystem.h
+++ 05-SceneManager/ParticleSystem.h
@@ -3,21 +3,26 @@
 
 #include "debug.h"
 //Simple particle system that allow to shoot out n particle similiar to fireworks.\
-
+///Particle stuffs
 #define PARTICLE_SPEED_Y 0.05f
 #define PARTICLE_SPEED_X 0.05f
-#define PARTICLE_SPEED_X_MAX 0.1f
-#define PARTICLE_SPEED_Y_MAX 0.1f
-
 
 #define PARTICLE_ACCEL_X 0.0005f //Counter force
 #define PARTICLE_ACCEL_Y 0.002f //Gravity
 
 #define PARTICLE_STARTING_FORCE 0.5f
-
 #define PARTICLE_LIVE_TIME 3000
+
+#define LPARTICLE Particle*
+#define LPPARTICLESYSTEM ParticleSystem*
+
+//Particle system stuffs
+
+
 class Particle : public CGameObject
 {
+
+
 	float ax, ay;
 	int spriteID = -1;
 	int aniID = -1;
@@ -56,7 +61,7 @@ public:
 	void OnNoCollision(DWORD dt)
 	{
 		vx = startSpeedX * direction.x;
-		vy = startSpeedY * direction.y;
+		vy = startSpeedY * abs(direction.y);
 
 		x += vx * dt;
 		y += vy * dt;
@@ -66,7 +71,7 @@ public:
 		if (startSpeedX < 0)
 			startSpeedX = 0;
 		startSpeedX -= ax * dt;
-		startSpeedY -= ay * dt;
+		startSpeedY += ay * dt;
 
 		if (GetTickCount64() - spawnTime > liveTime)
 		{
@@ -84,12 +89,34 @@ public:
 	void GetBoundingBox(float& left, float& top, float& right, float& bottom) {};
 };
 
+// X,y is supposed where the system will spawn. It can then use any object x,y to spawn particle as a center
+// Spawn a <count> number of particles in 4 main different direction ( Top left, top right, bottom left, bottom right). Each direction consist at least 1 particle, maximum of (count -3)
+// And will be randomized from 1 -> (count -3 - spawned) for each direction.
+class ParticleSystem : public CGameObject
+{
+	static ParticleSystem* __instance;
 
+	int particleCount;
+	int spawned = 0;
+	std::vector<Particle*> particles;
+	//How much force is pushed to the particle
+	float spawnPower = 1;
+public:
+	ParticleSystem(float x, float y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	int IsColliable() { return 0; }
+	int IsBlocking() { return 0; }
+	int SetSpawnPower (float spawnPower) { this->spawnPower = spawnPower; }
+	void SpawnStaticParticle(float x, float y, int count, int spriteID);
+	void SpawnAnimatedParticle(float x , float y,int count, int aniID);
 
-//
-//class ParticleSystem : public CGameObject
-//{
-//	int particleCount;
-//	int spriteID;
-//	std::list<Particle*> particles;
-//};
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+	void Render() {  };
+	void GetBoundingBox(float& left, float& top, float& right, float& bottom) {  };
+
+	static LPPARTICLESYSTEM GetInstance();
+
+};
